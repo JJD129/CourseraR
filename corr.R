c2d63d9639badc0e35501c21456547381a8c00de
JhenDi <zhenjhen192@gmail.com>
2020-05-07 18:16:36 -0500

    changed everything

--- corr.R
+++ corr.R
@@ -1,5 +1,16 @@
-directory <- getwd()
+specdata<-getwd()
 
 corr <- function(directory, threshold = 0){
+  correlation <- numeric()
   
+  for (file in list.files(directory)) {
+    df <- read.csv(file.path(directory, file), header=TRUE)
+    completeCases <- df[complete.cases(df),]
+    if(nrow(completeCases) > threshold){
+      sulfate <- completeCases[, "sulfate"]
+      nitrate <- completeCases[, "nitrate"]
+      correlation <- c(correlation, cor(sulfate, nitrate))
+    }
+  }
+  correlation
 }
