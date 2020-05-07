38a23aacd1a8b6e26406baa760b8e5618a920a8c
JhenDi <zhenjhen192@gmail.com>
2020-05-07 16:14:47 -0500

    changed filelist to directory set wd to specdata

--- complete.R
+++ complete.R
@@ -1,3 +1,5 @@
+specdata<-getwd()
+
 complete <- function(directory, id = 1:332)   {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
@@ -13,12 +15,21 @@ complete <- function(directory, id = 1:332)   {
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
   
-  fileList <- list.files(path=getwd(), pattern = ".csv", full.names = TRUE)
+  fileList <- list.files(path=directory, pattern = ".csv", full.names = TRUE) # getting all files
   nobs <- numeric()
   
   for (i in id){
-    data<-read.csv(fileList[i])
+    data <- read.csv(fileList[i])
     nobs <-c(nobs, sum(complete.cases(data)))
   }
-  
+
+  data.frame(id,nobs)
 }
+
+
+ ## test
+
+complete(specdata, 1)
+complete(specdata, c(2, 4, 8, 10, 12))
+complete(specdata, 30:25)
+complete(specdata, 3)
